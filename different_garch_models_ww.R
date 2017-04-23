cc853461c7bb8d8991e63c1492452378ed95d1e7
Veronika <starinska.veronika@gmail.com>
2017-04-23 17:55:02 +0200

    Add files via upload

---
+++ different_garch_models_ww.R
@@ -0,0 +1,77 @@
+library(rugarch)
+
+# Data import
+date_price <- read.csv("data_ww.csv", sep=",")
+#since 01/01/2011 - 31/12/2016
+price <- date_price[121:1685,2]
+log_r <- ts(diff(log(price)), start=c(2011), frequency = 261)
+
+############################################################################      
+# The GARCH(1,1) model     
+#########################################################################
+spec = ugarchspec(# variance equation
+  variance.model=list(model="sGARCH",garchOrder=c(1,1)),
+  # mean equation
+  mean.model=list(armaOrder=c(0,0),include.mean=F), 
+  # assumed distribution of errors
+  distribution.model="norm")
+
+k.garch11 = ugarchfit( spec=spec, data=na.omit(log_r))
+k.garch11
+
+# Plot of conditional standard deviation estimates (3)
+# and News-Impact curve (12).
+plot(k.garch11)
+ ############################################################################      
+# The GARCH(1,2) model     
+#########################################################################
+spec = ugarchspec(# variance equation
+  variance.model=list(model="sGARCH",garchOrder=c(1,2)),
+  # mean equation
+  mean.model=list(armaOrder=c(0,0),include.mean=F), 
+  # assumed distribution of errors
+  distribution.model="norm")
+
+k.garch12 = ugarchfit( spec=spec, data=na.omit(log_r))
+k.garch12
+plot(k.garch12)
+
+############################################################################      
+# The EGARCH model     
+#########################################################################
+# Let's examine whether conditional variance reacts asymmetrically 
+# to the news arriving to the market.
+# Below estimation of the EGARCH(1,1) model.
+
+# lets first define a model specification
+spec = ugarchspec(
+  variance.model=list(model="eGARCH",garchOrder=c(1,2)),
+  mean.model=list(armaOrder=c(0,0),include.mean=F), 
+  distribution.model="norm")
+
+# function doesn't accept missing values
+k.egarch11 = ugarchfit( spec=spec, data=na.omit(log_r))
+k.egarch11
+plot(k.egarch11)
+
+##########################################################################                                   
+# The TGARCH model
+###################################################################
+spec = ugarchspec(
+  variance.model=list(model="fGARCH",garchOrder=c(1,1),
+                      submodel="TGARCH"), #model="fGARCH" (family GARCH) together with submodel="TGARCH"
+  mean.model=list(armaOrder=c(0,0),include.mean=F), 
+  distribution.model="norm")
+
+k.tgarch11 = ugarchfit( spec=spec, data=na.omit(log_r))
+k.tgarch11
+plot(k.tgarch11)
+
+#### NIC
+par(mfrow=c(3,1))
+plot( k.garch11, which = 12)
+plot(k.egarch11, which = 12)
+plot( k.tgarch11, which = 12)
+par(mfrow=c(1,1))
+
+
---
+++ in_latex_final_ww.R
@@ -0,0 +1,71 @@
+#https://www.quantstart.com/articles/Generalised-Autoregressive-Conditional-Heteroskedasticity-GARCH-p-q-Models-for-Time-Series-Analysis
+library(tseries)
+library(MASS)
+library(stats)
+library(fGarch)
+library(astsa)
+library(TSA)
+library(forecast)
+
+## 2011-2016
+date_price <- read.csv("data_ww.csv", sep=",")
+price <- ts(date_price[121:1685,2], start = c(2011), frequency = 261)
+log_r <- ts(diff(log(date_price[121:1685,2])), start = c(2011), frequency = 261)
+
+## price vs. returns
+par(mfrow=c(2,1))
+plot(price,type='l') ### ts()...
+plot(log_r, type='l')
+#abline(0,0, col='green')
+
+
+mean(log_r)
+sd(log_r)
+skewness(log_r)
+kurtosis(log_r)
+
+## without ts()
+a <- diff(log(date_price[121:1685,2]))
+par(mfcol=c(2,1))
+plot(log_r, ylab = "Log Returns", main='Daily Log Returns of Bitcoin')
+acf((a-mean(a))^2, main = "ACF of Squared Mean-Adjusted Log Returns")
+par(mfcol=c(1,1))
+
+lr <- as.numeric(log_r)
+lr <- lr[!is.na(lr)]
+
+lrfinal.aic <- Inf
+lrfinal.order <- c(0,0,0)
+for (p in 1:4) for (d in 0:1) for (q in 1:4) {
+  lrcurrent.aic <- AIC(arima(lr, order=c(p, d, q)))
+    if (lrcurrent.aic < lrfinal.aic) {
+      lrfinal.aic <- lrcurrent.aic
+      lrfinal.order <- c(p, d, q)
+      lrfinal.arima <- arima(lr, order=lrfinal.order)
+      }
+}
+
+
+lrfinal.order ## ARIMA(3,0,4)
+plot(acf(resid(lrfinal.arima)), ylim = c(-0.1,0.5), main = "ACF of Residuals of an ARIMA(3,0,4)")
+plot(acf(resid(lrfinal.arima)^2), ylim = c(-0.1,0.5), main = "ACF of Squared Residuals of an ARIMA(3,0,4)") ## arch effect
+Box.test(resid(lrfinal.arima)^2, type = "Ljung-Box")
+
+lr.garch <- garch(lr, trace=F)
+lr.res <- lr.garch$res[-1]
+par(mfcol=c(2,1))
+plot(acf(lr.res, na.action = na.pass, plot = FALSE), ylim = c(-0.1,0.5), main = "ACF of the GARCH Residuals")
+plot(acf(lr.res^2, na.action = na.pass, plot = FALSE), ylim = c(-0.1,0.5), main = "ACF of the Squared GARCH Residuals")
+par(mfcol=c(1,1))
+#############
+#############
+#############
+#############
+#############
+############# 
+
+
+
+
+
+
